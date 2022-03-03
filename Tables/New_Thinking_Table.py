3d8a7fb4a874b6d546df9ef951be3f0b8d6d7a84
sfbrenzel <sfbrenzel@gmail.com>
2022-03-03 12:11:17 -0500

    3rd Commit Archive Project

--- Tables/New_Thinking_Table.py
+++ Tables/New_Thinking_Table.py
@@ -1,2 +1,20 @@
-# merge databases together to create one larger database
-# Sort data in data base  
+import pandas as pd
+
+from Kassel_Archive_Table import df
+from ERHF_DArchive_TableEF1914 import df2
+
+#merge databases together to create one database
+
+NTTable = df.merge(df2, how='outer')
+
+#reorder by date 
+NTTable = NTTable.sort_values(by='Date')
+
+#Change index numbering 
+NTTable.index = range(len(NTTable))
+
+
+NTTable.to_csv('NTTable1.csv', index = False)
+
+
+ 
