ad3c189896f8e96f8c77564a262da317d421cf26
sfbrenzel <sfbrenzel@gmail.com>
2022-06-16 14:34:37 -0400

    Cleaning ERHDA

--- ERHFDA_Cleaning.py
+++ ERHFDA_Cleaning.py
@@ -1,52 +1,53 @@
 import pandas as pd
-from ERHFDA_Merge import erhdatable
 
-#import digital archive ERH-FR 1914 DA CSV File 
-df2 = erhdatable
+
+erhdatablecleaned = pd.read_csv('Merged_Tables/erhdatablecleaned6.16.22.csv')
 
 #Merge columns 
-df2['Sender'] = df2['W: Last'] + ", " + df2['W: First']
+erhdatablecleaned['Sender'] = erhdatablecleaned['W: Last'] + ", " + erhdatablecleaned['W: First']
 
-df2['Recipient'] = df2['R: Last'] + ", " + df2['R: First']
+erhdatablecleaned['Recipient'] = erhdatablecleaned['R: Last'] + ", " + erhdatablecleaned['R: First']
 
-df2['Sender Location'] = df2['L: City'] + ", " + df2['L: Country']
+erhdatablecleaned['Sender Location'] = erhdatablecleaned['L: City'] + ", " + erhdatablecleaned['L: Country']
 
-df2['Recipient Location'] = df2['R: City'] + ", " + df2['R: Country']
+erhdatablecleaned['Recipient Location'] = erhdatablecleaned['R: City'] + ", " + erhdatablecleaned['R: Country']
 
 #delete unnecessary columns
-df2.drop(['Item ID', 'File Name', 'W: First', 'W: Last', 'R: First', 'R: Last', 'L: Language', 'O/C/D/X', 'MS/TS/P', 'ERH', 'pages', 'MRH', 'pages.1', 'Other 1', 'Other 2', 'Other 3', 'To Others', 'pages.2', 'Total L', 'Total p', 'Enclosures', 'Note1', 'Note2', 'URLw/Bio,etc', 'R: City', 'R: Country', 'L: Country', 'L: City'], axis=1, inplace=True) 
+erhdatablecleaned.drop(['Item ID', 'File Name', 'W: First', 'W: Last', 'R: First', 'R: Last', 'L: Language', 'O/X/C/D', 'MS/TS', 'R: City', 'R: Country', 'L: Country', 'L: City'], axis=1, inplace=True) 
 
 #change names of columns 
-df2.rename(columns={'L: Date': 'Date', 'Archive Location': 'Archive', 'Description': 'Comments'})
+erhdatablecleaned.rename(columns={'L: Date': 'Date', 'Archive Location': 'Archive', 'Description': 'Comments'}, inplace=True)
 
 #reorder columns 
-df2 = df2.reindex(columns=['Sender', 'Recipient', 'Date', 'Sender Location', 'Recipient Location', 'Archive', 'Rauner Item ID', 'Copyright Holder', 'Comments'])
+erhdatablecleaned = erhdatablecleaned.reindex(columns=['Sender', 'Recipient', 'Date', 'Sender Location', 'Recipient Location', 'Archive', 'Rauner Item ID', 'Copyright Holder', 'Comments'])
+
 
 #add columns 
-df2.insert(7, 'Published', '')
+#erhdatablecleaned.insert(7, 'Published', '')
 
 #convert column types 
-df2['Archive'] = df2['Archive'].astype(str)
+#erhdatablecleaned['Archive'] = erhdatablecleaned['Archive'].astype(str)
 
-df2['Copyright Holder'] = df2['Copyright Holder'].astype(str)
+#erhdatablecleaned['Copyright Holder'] = erhdatablecleaned['Copyright Holder'].astype(str)
 
 #add DA to archive column
-df2 = df2.assign(Archive = 'ERHF Digital Archive' )
+#erhdatablecleaned = erhdatablecleaned.assign(Archive = 'ERHF Digital Archive' )
 
-#Replace NaN values 
-df2['Date'] = df2['Date'].fillna(0)
+#replace NaN values 
+#erhdatablecleaned['Date'] = erhdatablecleaned['Date'].fillna(0)
 
-#recognfigure date format
-df2['Date'] = pd.to_datetime(df2['Date'], errors='coerce')
+#convert simple dates
+#erhdatablecleaned['Date'] = pd.to_datetime(erhdatablecleaned['Date'], errors='coerce')
+
+#erhdatablecleaned['Date']= pd.to_numeric(erhdatablecleaned['Date'], errors='coerce')
 
 #delete empty rows 
-df2.drop(index=df2.index[13:22], axis=0)
+#erhdatablecleaned.drop(index=erhdatablecleaned.index[13:22], axis=0)
 
 #delete ALL duplicate rows 
-df2.drop_duplicates(subset='Date', keep = 'first')
+#erhdatablecleaned.drop_duplicates(subset='Date', keep = 'first')
 
 #Change index numbering 
-df2.index = range(len(df2))
+#erhdatablecleaned.index = range(len(erhdatablecleaned))
 
-
-df2.to_csv('test1.csv')
+erhdatablecleaned.to_csv('test1.csv')
