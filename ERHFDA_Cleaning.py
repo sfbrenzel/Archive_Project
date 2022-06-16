d0b50fa6ef6e5ad6b9f6d981ab54f020ffc88fff
sfbrenzel <sfbrenzel@gmail.com>
2022-06-16 17:26:09 -0400

    Cleaning Updates

--- ERHFDA_Cleaning.py
+++ ERHFDA_Cleaning.py
@@ -1,4 +1,6 @@
+from sqlite3 import Date
 import pandas as pd
+from pyparsing import Regex
 
 
 erhdatablecleaned = pd.read_csv('Merged_Tables/erhdatablecleaned6.16.22.csv')
@@ -13,41 +15,39 @@ erhdatablecleaned['Sender Location'] = erhdatablecleaned['L: City'] + ", " + erh
 erhdatablecleaned['Recipient Location'] = erhdatablecleaned['R: City'] + ", " + erhdatablecleaned['R: Country']
 
 #delete unnecessary columns
-erhdatablecleaned.drop(['Item ID', 'File Name', 'W: First', 'W: Last', 'R: First', 'R: Last', 'L: Language', 'O/X/C/D', 'MS/TS', 'R: City', 'R: Country', 'L: Country', 'L: City'], axis=1, inplace=True) 
+erhdatablecleaned.drop(['Item ID', 'W: First', 'W: Last', 'R: First', 'R: Last', 'L: Language', 'O/X/C/D', 'MS/TS', 'R: City', 'R: Country', 'L: Country', 'L: City'], axis=1, inplace=True) 
 
 #change names of columns 
 erhdatablecleaned.rename(columns={'L: Date': 'Date', 'Archive Location': 'Archive', 'Description': 'Comments'}, inplace=True)
 
 #reorder columns 
-erhdatablecleaned = erhdatablecleaned.reindex(columns=['Sender', 'Recipient', 'Date', 'Sender Location', 'Recipient Location', 'Archive', 'Rauner Item ID', 'Copyright Holder', 'Comments'])
-
+erhdatablecleaned = erhdatablecleaned.reindex(columns=['File Name', 'Sender', 'Recipient', 'Date', 'Sender Location', 'Recipient Location', 'Archive', 'Rauner Item ID', 'Copyright Holder', 'Comments'])
 
 #add columns 
-#erhdatablecleaned.insert(7, 'Published', '')
+erhdatablecleaned.insert(7, 'Published', '')
 
 #convert column types 
-#erhdatablecleaned['Archive'] = erhdatablecleaned['Archive'].astype(str)
+erhdatablecleaned['Archive'] = erhdatablecleaned['Archive'].astype(str)
 
-#erhdatablecleaned['Copyright Holder'] = erhdatablecleaned['Copyright Holder'].astype(str)
+erhdatablecleaned['Copyright Holder'] = erhdatablecleaned['Copyright Holder'].astype(str)
 
 #add DA to archive column
-#erhdatablecleaned = erhdatablecleaned.assign(Archive = 'ERHF Digital Archive' )
+erhdatablecleaned = erhdatablecleaned.assign(Archive = 'ERHF Digital Archive' )
 
 #replace NaN values 
-#erhdatablecleaned['Date'] = erhdatablecleaned['Date'].fillna(0)
+erhdatablecleaned['Date'] = erhdatablecleaned['Date'].fillna(0)
+
+#replace unknown values
+erhdatablecleaned = erhdatablecleaned.replace({'Date': 'unknown'}, {'Date': ''})
 
 #convert simple dates
-#erhdatablecleaned['Date'] = pd.to_datetime(erhdatablecleaned['Date'], errors='coerce')
+erhdatablecleaned['Date'] = pd.to_datetime(erhdatablecleaned['Date'], errors='coerce')
 
-#erhdatablecleaned['Date']= pd.to_numeric(erhdatablecleaned['Date'], errors='coerce')
-
-#delete empty rows 
-#erhdatablecleaned.drop(index=erhdatablecleaned.index[13:22], axis=0)
 
 #delete ALL duplicate rows 
-#erhdatablecleaned.drop_duplicates(subset='Date', keep = 'first')
+#erhdatablecleaned.drop_duplicates(subset='Date', keep = 'first', inplace=True)
 
 #Change index numbering 
-#erhdatablecleaned.index = range(len(erhdatablecleaned))
+erhdatablecleaned.index = range(len(erhdatablecleaned))
 
 erhdatablecleaned.to_csv('test1.csv')
